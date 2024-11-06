4c1b8394c79f7e6df824fe34513cfb37cdb4b401
= <alexander.schaefer@uvm.edu>
2024-11-06 09:53:48 -0500

    3011. Find if Array Can Be Sorted

---
+++ 3011. Find if Array Can Be Sorted/README.md
@@ -0,0 +1,3 @@
+# 3011. Find if Array Can Be Sorted
+## Language: Python
+Compare every element in the list to the elements to its left. If an element to its left cannot be swapped and would need to be, return False. Else return True.
---
+++ 3011. Find if Array Can Be Sorted/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canSortArray(self, nums: List[int]) -> bool:
+        temp_val = 0
+        for i in range(len(nums)):
+            for j in range(i):
+                if i == 0:
+                    pass
+                elif nums[i] < nums[j]:
+                    if not (str(bin(nums[i])).count("1") == str(bin(nums[j])).count("1")):
+                        return False
+        return True
+
